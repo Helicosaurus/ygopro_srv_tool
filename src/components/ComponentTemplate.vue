b44ef42c6df2da45f5289dc4df6316b508c12af4
Helicosaurus <enaryok@gmail.com>
2019-08-22 21:47:50 +0200

    Add Vuetify

---
+++ src/components/ComponentTemplate.vue
@@ -0,0 +1,22 @@
+<!-- Add html here -->
+<template>
+  
+</template>
+
+<!-- Javascript  -->
+<script>
+export default {
+  name: 'ComponentTemplate',
+  data: {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
